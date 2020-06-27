937eed7665cf87cd00e01847025e79f642de1c9f
JeyXeon <jeyxxs@gmail.com>
2020-06-27 18:45:24 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    static Set <Socket> clients = new HashSet<>();
+
+    static synchronized void addClient(Socket client){
+        clients.add(client);
+    }
+
+    static synchronized void removeClient(Socket client){
+        clients.remove(client);
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = new ServerSocket(8083);
+        while (true) {
+            Socket accept = ss.accept();
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        addClient(accept);
+                        InputStream inputStream = accept.getInputStream();
+                        OutputStream outputStream = accept.getOutputStream();
+                        BufferedReader br =
+                                new BufferedReader(new InputStreamReader(inputStream));
+                        while (true) {
+                            String line = br.readLine();
+                            if (line == null || "exit".equals(line)) break;
+
+                            for (Socket s: clients){
+                                System.out.println(accept.getRemoteSocketAddress() +
+                                        " Got line: " + line);
+                                try {
+                                    s.getOutputStream().write((line + "\n").getBytes());
+                                } catch (IOException e){
+                                }
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    removeClient(accept);
+                }
+            }).start();
+
+            clients.add(accept);
+
+        }
+    }
+}
